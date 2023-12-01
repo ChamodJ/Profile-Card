27d6d84a3d067f7686d03e1f9c6b86921e1f46e4
Chamod Perera <chamod914@gmail.com>
2023-12-01 13:41:30 +0530

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.html"],
+  theme: {
+    extend: {
+      colors : {
+        'Dark-cyan': 'hsl(185, 75%, 39%)',
+        'Very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+        'Dark-grayish-blue' : 'hsl(227, 10%, 46%)'
+      }
+    },
+  },
+  plugins: [],
+}
+
