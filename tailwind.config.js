3cdec909659a4d86262c8ee6f51987e176d9a596
Chamod Perera <chamod914@gmail.com>
2023-12-01 23:29:10 +0530

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,19 @@ module.exports = {
     fontFamily : {
       'Kumbh-Sans' : ['Kumbh Sans', 'sans-serif']
     },
-
+    
     extend: {
       colors : {
         'Dark-cyan': 'hsl(185, 75%, 39%)',
         'Very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
         'Dark-grayish-blue' : 'hsl(227, 10%, 46%)',
         'Dark-gray': 'hsl(0, 0%, 59%)'
-      }
+      },
+
+      maxWidth: {
+        '500': '500px',
+      },
+
     },
   },
   plugins: [],
